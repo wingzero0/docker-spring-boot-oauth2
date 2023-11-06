68df5ee74d8fe94a8d1f1c32dd495e08fd325541
wingzero0 <wingzzz2003@hotmail.com>
2023-11-06 02:05:26 +0000

    test front request pass to backend and view from gitpod forwarded domain

--- ssomanagement/frontend/vite.config.ts
+++ ssomanagement/frontend/vite.config.ts
@@ -15,5 +15,11 @@ export default defineConfig({
   },
   server: {
     host: "0.0.0.0",
+    proxy: {
+      '^/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+    }
   }
 })
