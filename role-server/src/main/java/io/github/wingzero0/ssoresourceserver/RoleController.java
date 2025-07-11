d5801efe2c1f1084788e76676a6d56bc290975d4
wingzero0 <wingzzz2003@hotmail.com>
2025-07-11 10:39:21 +0800

    revise role-server /api/appRole, limit to user access token. only return app role related to that user and client app

--- role-server/src/main/java/io/github/wingzero0/ssoresourceserver/RoleController.java
+++ role-server/src/main/java/io/github/wingzero0/ssoresourceserver/RoleController.java
@@ -1,12 +1,12 @@
 package io.github.wingzero0.ssoresourceserver;
 
+import io.github.wingzero0.ssoentity.entity.AppUserRole;
+import io.github.wingzero0.ssoentity.repo.AppUserRoleRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.server.resource.authentication.BearerTokenAuthentication;
 import org.springframework.web.bind.annotation.*;
-
-import io.github.wingzero0.ssoentity.entity.AppUserRole;
-import io.github.wingzero0.ssoentity.repo.AppUserRoleRepository;
 
 import java.security.Principal;
 import java.util.HashMap;
@@ -17,7 +17,7 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/api")
 public class RoleController {
-    private static Logger LOG = LoggerFactory.getLogger(RoleController.class);
+    private static final Logger LOG = LoggerFactory.getLogger(RoleController.class);
 
     @Autowired
     private AppUserRoleRepository appUserRoleRepo;
@@ -25,7 +25,7 @@ public class RoleController {
     @GetMapping("/testScopeRead")
     @ResponseBody
     public Map<String, Object> user(Principal principal) {
-        Map<String, Object> ret = new HashMap<> ();
+        Map<String, Object> ret = new HashMap<>();
         ret.put("principal", principal);
         ret.put("message", "it works");
         return ret;
@@ -34,28 +34,61 @@ public class RoleController {
     @GetMapping("/testScopeWrite")
     @ResponseBody
     public Map<String, Object> testDB(Principal principal) {
-        Map<String, Object> ret = new HashMap<> ();
+        Map<String, Object> ret = new HashMap<>();
         ret.put("principal", principal);
         ret.put("message", "it works");
         return ret;
     }
 
+    // for user credential access token
     @GetMapping("/appRole")
-    public List<AppUserRole> getAllRoleInApp(Principal principal) {
-        return appUserRoleRepo.findAllByAppClientId(principal.getName());
+    public List<String> getRoleByUserToken(Principal principal) {
+        LOG.debug("principal class:{}", principal.getClass().getCanonicalName());
+        if (principal instanceof BearerTokenAuthentication auth) {
+            Map<String, Object> tokenAttributes = auth.getTokenAttributes();
+            tokenAttributes.forEach((key, value) -> {
+                LOG.debug("key:{}", key);
+                LOG.debug("value:{}", value.toString());
+                LOG.debug("value class:{}", value.getClass().getCanonicalName());
+            });
+            String clientId = null;
+            if (tokenAttributes.containsKey("client_id")) {
+                clientId = (String) tokenAttributes.get("client_id");
+            }
+            String username = null;
+            if (tokenAttributes.containsKey("sub")) {
+                username = (String) tokenAttributes.get("sub");
+            }
+            List<String> roles = appUserRoleRepo.findAllByAppClientIdAndUsername(clientId, username)
+                    .stream()
+                    .map(AppUserRole::getUserRole)
+                    .collect(Collectors.toList());
+            for (String role : roles) {
+                LOG.debug(role);
+            }
+            return roles;
+        }
+        return List.of();
     }
 
+    // for client credential access token
     @GetMapping("/appRole/{username}")
-    public List<String> getAllRoleInAppByUsername(Principal principal,
-            @PathVariable(name = "username") String username) {
-        LOG.debug("principal name:" + principal.getName() + " username:" + username);
+    public List<String> getRoleByClientTokenAndUsername(Principal principal,
+                                                        @PathVariable(name = "username") String username) {
+        LOG.debug("principal name:{}, username:{}", principal.getName(), username);
         List<String> roles = appUserRoleRepo.findAllByAppClientIdAndUsername(principal.getName(), username)
                 .stream()
-                .map(appUserRole -> appUserRole.getUserRole())
+                .map(AppUserRole::getUserRole)
                 .collect(Collectors.toList());
-        for (String role : roles){
+        for (String role : roles) {
             LOG.debug(role);
         }
         return roles;
+    }
+
+    // for client credential access token
+    @GetMapping("/appRoles")
+    public List<String> getRoleByClientToken(Principal principal) {
+        return appUserRoleRepo.findAllByAppClientId(principal.getName()).stream().map(AppUserRole::getUserRole).toList();
     }
 }
